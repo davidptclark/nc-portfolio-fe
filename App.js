6aa501f44b1796a68acb06bf445cad5b638a8d54
Ben Lomas <benlomas7@outlook.com>
2022-03-21 11:50:44 +0000

    Added login function that changes user

--- App.js
+++ App.js
@@ -2,12 +2,17 @@ import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import styles from "./src/styles/Styles";
 import Login from "./src/components/Login";
+import { UserContext } from "./src/contexts/UserContext";
+import { useState } from "react";
 
 export default function App() {
+  const [user, setUser] = useState({});
   return (
-    <View style={styles.container}>
-      <Login />
-      <StatusBar style="auto" />
-    </View>
+    <UserContext.Provider value={{ user, setUser }}>
+      <View style={styles.container}>
+        <Login />
+        <StatusBar style="auto" />
+      </View>
+    </UserContext.Provider>
   );
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,48 @@
-import { Text, TextInput, View } from "react-native-web";
+import { Button, Text, TextInput, View } from "react-native-web";
+import { useContext, useState } from "react";
+import { UserContext } from "../contexts/UserContext";
 import styles from "../styles/Styles";
 
 const Login = () => {
+  const [newUser, setNewUser] = useState({ username: "", password: "" });
+  const { setUser } = useContext(UserContext);
   return (
     <View>
       <Text style={styles.text}>Log in</Text>
       <Text style={styles.text}>Username</Text>
-      <TextInput style={styles.textInput} />
+      <TextInput
+        style={styles.textInput}
+        placeholder="Username"
+        onChangeText={(newUsername) =>
+          setUser((currentUser) => {
+            const newUser = { ...currentUser };
+            newUser.username = newUsername;
+            return newUser;
+          })
+        }
+      />
       <Text style={styles.text}>Password</Text>
-      <TextInput style={styles.textInput} />
+      <TextInput
+        style={styles.textInput}
+        placeholder="Password"
+        onChangeText={(newPassword) =>
+          setUser((currentUser) => {
+            const newUser = { ...currentUser };
+            newUser.password = newPassword;
+            return newUser;
+          })
+        }
+        secureTextEntry
+      />
+      <Button
+        onPress={() => {
+          setUser(newUser);
+          setNewUser({ username: "", password: "" });
+        }}
+        title="Login"
+        color="#841584"
+        accessibilityLabel="log in"
+      />
     </View>
   );
 };
