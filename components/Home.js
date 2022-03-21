fba66e20f73055e43fe6687252e20b2ee4573cf6
quarki <62488270+aasaezm@users.noreply.github.com>
2022-03-21 12:05:24 +0000

    Home component initial setup

---
+++ components/Home.js
@@ -0,0 +1,17 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default Home = () => {
+  return (
+    <View style={homeContainer}>
+      {/* 
+    <Header />
+    <SortBar />
+    <VideoFeed />  
+  */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  homeContainer: {},
+});
