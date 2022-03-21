2269ab690d6010ab9ca5c0c5f1516b874f495a99
David Clark <david.p.t.clark@gmail.com>
2022-03-21 12:35:34 +0000

    Add boilerplate Upload components moved to src/components

---
+++ src/components/Upload.jsx
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default Upload = () => {
+  return (
+    <View style={uploadContainer}>
+      {/* 
+    <Header />
+    <DropZone />
+    <TextForm (for bio, tags etc.) />
+    <ProgressBar />  
+  */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  uploadContainer: {},
+});
