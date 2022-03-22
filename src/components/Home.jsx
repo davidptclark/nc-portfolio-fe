67c8a3512b3d2d34d466045dd48ad0518d46228b
Ben Lomas <benlomas7@outlook.com>
2022-03-22 12:52:08 +0000

    Added dropdown to main page to filter tags

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,9 +5,23 @@ import { useEffect, useState, useRef } from "react";
 import { FlatList } from "react-native";
 import axios from "axios";
 import styles from "../styles/Styles";
+import DropDownPicker from "react-native-dropdown-picker";
 
 export default Home = () => {
   const [videos, setVideos] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [tags, setTags] = useState([]);
+  const [items, setItems] = useState([
+    { label: "Language", value: "language" },
+    { label: "Javascript", value: "javascript", parent: "language" },
+    { label: "Python", value: "python", parent: "language" },
+
+    { label: "Front End", value: "frontEnd" },
+    { label: "React", value: "react", parent: "frontEnd" },
+    { label: "React Native", value: "reactNative", parent: "frontEnd" },
+    { label: "Back End", value: "backEnd" },
+    { label: "Express", value: "express", parent: "backEnd" },
+  ]);
   const cld = new Cloudinary({
     cloud: {
       cloudName: "ncfiveguysuk",
@@ -25,6 +39,17 @@ export default Home = () => {
 
   return (
     <View style={styles.container}>
+      <DropDownPicker
+        searchable
+        placeholder="Tags"
+        open={open}
+        value={tags}
+        items={items}
+        setOpen={setOpen}
+        setValue={setTags}
+        setItems={setItems}
+        multiple
+      />
       <FlatList
         data={videos.map((video) => {
           return {
