7c8b7ef696ce21de515e53f192809700dc8686a7
David Clark <david.p.t.clark@gmail.com>
2022-03-22 10:55:42 +0000

    Add new bare UserPage component

---
+++ src/components/UserPage.jsx
@@ -0,0 +1,8 @@
+import { Text, View } from "react-native";
+export default UserPage = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Welcome to your profile</Text>
+    </View>
+  );
+};
