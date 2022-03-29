3f96eeafe2146a1b4c3c204689b8e53b57ae1d21
Tom wallwin <TWallwin>
2022-03-29 14:30:45 +0100

    set upload property to videos only

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -29,7 +29,7 @@ export default Upload = () => {
   const pickVideo = async () => {
     // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      mediaTypes: ImagePicker.MediaTypeOptions.Videos,
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
