9ba9a231a26aa8235979d6ad3b408b70e921fcd8
Tom wallwin <TWallwin>
2022-03-29 14:10:28 +0100

    started adding delete

--- src/utils/api.js
+++ src/utils/api.js
@@ -12,17 +12,41 @@ export const patchVotesByVideoId = (vote, video_id) => {
   return api.patch(`/videos/${video_id}`, { vote: vote });
 };
 
-export const postCloudinary = (url, formData, tags) => {
-  return fetch(url, {
+export const postCloudinary = (url, formData) => {
+  return fetch(url + `/upload`, {
     method: "post",
     body: formData,
   }).then((res) => res.json());
 };
-
-export const postVideoToDatabase = (returnData) => {
-  return api.post("/videos", returnData).then(() => {});
+export const deleteCloudinary = (url, formData) => {
+  return fetch(url + `/delete_by_token`, {
+    method: "post",
+    body: { token: delete_token },
+  })
+    .then((res) => {
+      console.log(res);
+      console.log("video deleted");
+    })
+    .catch(console.log);
 };
 
+export const postVideoToDatabase = (returnData, url, formData) => {
+  console.log(returnData);
+  // return api
+  //   .post("/videos", returnData)
+  //   .then(() => {
+  //     console.log("posted");
+  //   })
+
+  return Promise.reject({ msg: "post failed" }).catch((err) => {
+    console.log(err);
+    return deleteCloudinary(url, formData);
+  });
+};
+
+export const GetCommentsByVideoId = (video_id) => {
+  return api.get(`/comments/${video_id}`);
+};
 export const signinUser = (username, password) => {
   return api
     .post("/signin", { username, password })
@@ -31,6 +55,14 @@ export const signinUser = (username, password) => {
     });
 };
 
-export const GetCommentsByVideoId = (video_id) => {
-  return api.get(`/comments/${video_id}`);
+export const postUser = (username, password, type) => {
+  return api
+    .post("/users", { username, password, type })
+    .then(({ data: { user } }) => {
+      return user;
+    });
+};
+
+export const patchUser = ({ username, bio, avatar_url, social_url }) => {
+  return api.patch(`/users/${username}`, { bio, avatar_url, social_url });
 };
