0eabc2f3273740cb4edbf102a87f70fa107f6925
Thomas Wallwin <94870459+TWallwin@users.noreply.github.com>
2022-03-29 14:53:51 +0100

    removed duplicate functions

--- src/utils/api.js
+++ src/utils/api.js
@@ -61,14 +61,3 @@ export const getTags = () => {
   });
 };
 
-export const postUser = (username, password, type) => {
-  return api
-    .post("/users", { username, password, type })
-    .then(({ data: { user } }) => {
-      return user;
-    });
-};
-
-export const patchUser = ({ username, bio, avatar_url, social_url }) => {
-  return api.patch(`/users/${username}`, { bio, avatar_url, social_url });
-};
