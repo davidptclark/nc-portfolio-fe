18a45adb044fa14a11bd7c5cd7725680349ba1a2
David Clark <david.p.t.clark@gmail.com>
2022-03-30 15:40:21 +0100

    Add consistent colour scheme with buttons and tab bar

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     borderRadius: 50,
     elevation: 3,
-    backgroundColor: "blue",
+    backgroundColor: "purple",
     paddingTop: 10,
     paddingBottom: 10,
     marginBottom: 5,
