ee4c83ee118b30c97066bb8975ed1063a22eccc8
Jorge-Figueira <xurxo.figueira@gmail.com>
2022-03-30 20:33:03 +0100

    Adds the default avatar to display if the user does not have one

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -5,7 +5,7 @@ import styles from "../styles/Styles";
 
 export default AvatarUrl = ({ username }) => {
   console.log(username, "username");
-  const [url, setUrl] = useState("");
+  const [url, setUrl] = useState("default");
   useEffect(() => {
     api.GetUserByName(username).then((result) => {
       setUrl(result.data.user.avatar_url);
@@ -14,12 +14,18 @@ export default AvatarUrl = ({ username }) => {
 
   return (
     <View>
-      {url !== "" && (
+      {url !== "default" && (
         <Image
           style={styles.avatar_image}
           source={{
             uri: url,
           }}
+        />
+      )}
+      {url === "default" && (
+        <Image
+          style={styles.avatar_image}
+          source={require("../../defaultAvatar.png")}
         />
       )}
     </View>
